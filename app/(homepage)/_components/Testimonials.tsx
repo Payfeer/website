ed4d8d5240f375983e70e406d47fd646fc1739eb
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-11-18 03:38:14 +0100

    carousel

--- app/(homepage)/_components/Testimonials.tsx
+++ app/(homepage)/_components/Testimonials.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useRef } from "react";
 import CTA from "./CTA";
 
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Testimonial1 from "@/public/assets/images/person.png";
 import Image from "next/image";
-import { Star } from "lucide-react";
 import { BsStarFill } from "react-icons/bs";
 
 const testimonials = [
@@ -43,18 +42,18 @@ const testimonials = [
 ];
 
 const Testimonials = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
-    );
-  };
+  const scroll = (direction: "left" | "right") => {
+    if (scrollRef.current) {
+      const { scrollLeft, clientWidth } = scrollRef.current;
+      const scrollAmount = direction === "left" ? -clientWidth : clientWidth;
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1
-    );
+      scrollRef.current.scrollTo({
+        left: scrollLeft + scrollAmount,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
@@ -67,41 +66,68 @@ const Testimonials = () => {
           <h2 className="font-semibold text-xl md:text-3xl">
             What Our Users Are Saying
           </h2>
-          <p className="max-w-[900px] text-sm md:text-lg/relaxed">
-            Discover why Payfeer is the preferred choice for seamless currency
-            exchange and secure transfers. Hear directly from our users about
-            their experiences managing funds with ease and confidence.
-          </p>
+
+          {/* Scroll Buttons */}
+          <div className="flex gap-8 justify-between w-full">
+            <p className="max-w-[731px] text-sm md:text-lg/relaxed md:text-left">
+              Discover why Payfeer is the preferred choice for seamless currency
+              exchange and secure transfers. Hear directly from our users about
+              their experiences managing funds with ease and confidence.
+            </p>
+
+            <div className="hidden md:flex items-center gap-8">
+              <button
+                onClick={() => scroll("left")}
+                className="z-10 bg-[#D8D8D8] size-14 flex items-center justify-center p-1 rounded-full shadow-md hover:bg-gray-200 active:bg-payfeer-primary active:text-white transition-all"
+              >
+                <FaChevronLeft className="size-5" />
+                <span className="sr-only">Left</span>
+              </button>
+
+              <button
+                onClick={() => scroll("right")}
+                className="z-10 bg-[#D8D8D8] size-14 flex items-center justify-center p-1 rounded-full shadow-md hover:bg-gray-200 active:bg-payfeer-primary active:text-white transition-all"
+              >
+                <FaChevronRight className="size-5" />
+                <span className="sr-only">Right</span>
+              </button>
+            </div>
+          </div>
         </div>
 
-        <div className="relative flex gap-6 items-center overflow-scroll">
-          {testimonials.map((testimonial, index) => (
-            <div key={testimonial.id}>
-              <div className="flex flex-col gap-4 bg-white p-6 rounded-lg shadow-lg w-[330px]">
-                <div className="flex gap-1">
-                  {Array(5)
-                    .fill("")
-                    .map((_, i) => (
-                      <span key={i} className="text-[#E9D206] text-base">
-                        <BsStarFill />
-                      </span>
-                    ))}
-                </div>
-                <p className="text-sm">{testimonial.text}</p>
-                <div className="flex items-center gap-3">
-                  <Image
-                    src={testimonial.image}
-                    alt={testimonial.name}
-                    className="size-[49px] rounded-xl"
-                    width={49}
-                    height={49}
-                    quality={90}
-                  />
-                  <span className="font-semibold">{testimonial.name}</span>
+        <div className="flex items-center">
+          <div
+            ref={scrollRef}
+            className="flex gap-6 items-center overflow-x-scroll scroll-smooth scrollbar-hide w-full"
+          >
+            {testimonials.map((testimonial) => (
+              <div key={testimonial.id}>
+                <div className="flex flex-col gap-4 bg-white p-6 rounded-lg shadow-lg w-[330px]">
+                  <div className="flex gap-1">
+                    {Array(5)
+                      .fill("")
+                      .map((_, i) => (
+                        <span key={i} className="text-[#E9D206] text-base">
+                          <BsStarFill />
+                        </span>
+                      ))}
+                  </div>
+                  <p className="text-sm">{testimonial.text}</p>
+                  <div className="flex items-center gap-3">
+                    <Image
+                      src={testimonial.image}
+                      alt={testimonial.name}
+                      className="size-[49px] rounded-xl"
+                      width={49}
+                      height={49}
+                      quality={90}
+                    />
+                    <span className="font-semibold">{testimonial.name}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
